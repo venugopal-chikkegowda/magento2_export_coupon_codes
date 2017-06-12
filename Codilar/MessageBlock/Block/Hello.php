13c283d0954e4d8b18fa3fbef3fc8c0c87f0ecb1
venugopal_chikkegowda <venugopal@codilar.com>
2017-06-12 16:47:01 +0530

    useless module removed

--- Codilar/MessageBlock/Block/Hello.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Codilar\MessageBlock\Block;
-class Hello extends \Magento\Framework\View\Element\Template
-{
-    public function _prepareLayout()
-    {
-        return parent::_prepareLayout();
-    }
-}
--- Codilar/MessageBlock/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Codilar_MessageBlock',
-    __DIR__
-);
